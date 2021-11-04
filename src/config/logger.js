dd68ad80b813e86a44f5da4b5c48c680dff18f9c
Guillaume Forestier <gforestier2000@yahoo.fr>
2021-11-04 20:14:55 +0100

    Initialisation du projet

---
+++ src/config/logger.js
@@ -0,0 +1,27 @@
+//import pino from "pino"
+const pino = require('pino')
+
+// log levels system
+const levels = {
+  http: 10,
+  debug: 20,
+  info: 30,
+  warn: 40,
+  error: 50,
+  fatal: 60
+};
+
+console.log('process.env.LOGLEVEL : ' + process.env.LOGLEVEL);
+// create a Pino logger
+const logger = pino({
+  // set the own levels
+  customLevels: levels,
+  // use only the custom levels
+  useOnlyCustomLevels: true,
+  // the minimum log level to be display
+  level: process.env.LOGLEVEL || "error"
+});
+
+// export the logger
+// export default logger; <= marche pas
+module.exports = logger;
---
+++ src/main.js
@@ -0,0 +1,46 @@
+console.log("hello");
+require('dotenv').config();
+// prendre en compte l'environnement
+//require('dotenv').config({path: __dirname + '/.env'});
+
+const logger = require('./config/logger');
+
+logger.http("HTTP message");
+logger.debug("DEBUG message");
+logger.info("INFO message");
+logger.warn("WARN message");
+logger.error("ERROR message");
+logger.fatal("FATAL message");
+
+const MySQL = require('mysql');
+
+logger.debug("utilisateur avant MySQL.createConnection");
+logger.debug(`DB_HOST : ${process.env.DB_HOST}`);
+logger.debug(`DB_PORT : ${process.env.DB_PORT}`);
+logger.debug(`DB_USER : ${process.env.DB_USER}`);
+logger.debug(`DB_PASSWORD : ${process.env.DB_PASSWORD}`);
+logger.debug(`DB_DATABASE : ${process.env.DB_DATABASE}`);
+
+
+const mysqlConnection = MySQL.createConnection({
+    host: process.env.DB_HOST, //192.168.2.47
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+
+
+//const mysqlConnection = require('../config/dbconfig');
+
+logger.debug("utilisateur après MySQL.createConnection");
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        logger.info("DB connection succeed");
+    else {
+        logger.fatal("DB connection failed");
+        logger.fatal(err);
+    }
+});
